d2ae56acd80660eb3a8abf3a576c998a5c6fd426
Niyas <niyas75tk@gmail.com>
2024-12-04 15:07:26 +0530

    feat : Added vercel deployment

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const adminRoute = require('./routes/adminRoute');
 const errorHandler = require("./middleware/errorHandler");
 const app = express();
 mongoose
-  .connect("mongodb+srv://niyas75tk:sc6wk0Q3xmdjqXgK@cluster0.edtkkwg.mongodb.net/?tls=true/petshop")
+  .connect("mongodb+srv://niyas75tk:sc6wk0Q3xmdjqXgK@cluster0.edtkkwg.mongodb.net/?tls=true")
   .then(() => console.log("connected  to mongodb"))
   .catch(() => console.log("error while conecting to mongodb"));
 app.use(express.json());
